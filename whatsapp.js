245435e46b2e3e9a868eaecab3a6dc057f8cfdfd
Siddhant Sundar <siddhantsundar2016@gmail.com>
2024-03-21 01:26:27 +0530

    Update

--- whatsapp.js
+++ whatsapp.js
@@ -4,11 +4,25 @@ function sendToWhatsapp(){
 	let number2 = document.getElementById('number2').value;
 	let pages = document.getElementById('page').value;
 	let refcode = document.getElementById('ref').value;
-	let select = document.getElementById('selectedOption').value;
+	var select = document.getElementById('selectedOption').value;
 	let select2 = document.getElementById('argb').value;
-	let select3 = document.getElementById('argb2').value;
+	var select3 = document.getElementById('argb2').value;
 	let name2 = document.getElementById('name2').value;
 	let totalcost = pages*select3;
+	let length = select.length;
+
+	if (length=26){
+		rem=totalcost+60;
+	}
+	else if (length=30){
+		rem=totalcost+40;
+	}
+	else{
+		rem=totalcost+0;
+	}
+
+	
+
 	
 	var url = "https://wa.me/" +"+91"+ number2 + "?text="
 	+"*Customer Invoice*"+ "%0a"
@@ -23,8 +37,54 @@ function sendToWhatsapp(){
 	+ "*Outlets Available For Pickup and Drop*" +" "+select2+" "+ "%0a"
 	+ "*Submission Day:*" +" "+name2+" "+ "%0a"
 	+ "*Rate Per Page:*" +" "+select3+"Rs/Side"+" "+ "%0a"
-	+ "*Total Cost:*" +" "+totalcost+" "+ "%0a"
+	+ "*Total Cost:*" +" "+rem+" "+ "%0a"
 	+ "%0a"
 	+ "*For any issues feel free to contact us at: 8549013115 or 7892848358*" + "%0a"
 	window.open(url, '_blank').focus();
 }
+
+function Whatsapp(){
+
+	let name = document.getElementById('name').value;
+	let Rate_Writers = document.getElementById('Rate_Writers').value;
+	let number2 = document.getElementById('number2').value;
+	let pages = document.getElementById('page').value;
+	let refcode = document.getElementById('ref').value;
+	var select = document.getElementById('selectedOption').value;
+	let select2 = document.getElementById('argb').value;
+	let name2 = document.getElementById('name2').value;
+	let total_writer = pages*Rate_Writers;
+let length = select.length;
+
+
+if (length=26){
+	rem=total_writer+60;
+}
+else if (length=30){
+	rem=total_writer+40;
+}
+else{
+	rem=total_writer+0;
+}
+	var url = "https://wa.me/" +"+91" + number2 + "?text="
+	+"*Writer's Listing!*"+ "%0a"+ "%0a"
+
+	+"*Welcome to Quill Dynamics,Think precision, Think Quill Dynamics.*"+ "%0a" +"*New Order Details:*"+ "%0a" + "%0a"
+	+ "*Refrence Code:*"+" "+refcode+ "%0a"
+	+ "*Name :*" +" "+name+" "+ "%0a"
+	
+	+ "*Service Type:*" +" "+select+" "+ "%0a"
+
+	+"*Total pages :*" +" "+pages+" "+ "%0a"	
+
+	+ "*Submission Area:*" +" "+select2+" "+ "%0a"
+
+	+ "*Deadline of Completion:*" +" "+name2+" "+ "%0a"
+
+	+ "*Rate Per Page:*" +" "+Rate_Writers+"Rs/Side"+" "+ "%0a"
+
+	+ "*Total Cost:*" +" "+rem+" "+ "%0a"
+	+ "%0a"
+	+ "*For any issues feel free to contact us at: 8549013115 or 7892848358*" + "%0a"
+	window.open(url, '_blank').focus();
+}///*
